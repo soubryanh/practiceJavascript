ef1801e1f3c54ae12d321635745c6663f5995d5f
bryanhernandz102 <bryanhernandz102@gmail.com>
2021-07-01 19:40:01 -0500

    how to calculate a hypotenuse

--- figures.js
+++ figures.js
@@ -12,12 +12,26 @@ console.log(you);
 const areaTriangle = (base, height) => (base * height) / 2;
 
 //Circule
-
+const PI = Math.PI;
 const diameterCircle = (radio) => radio * 2;
 
-const perimeterCircle = (radio) => diameterCircle(radio) * Math.PI;
+const perimeterCircle = (radio) => diameterCircle(radio) * PI;
 
-const areaCircle = (radio) => radio * radio * Math.PI;
+const areaCircle = (radio) => radio * radio * PI;
+
+// isosceles Tringle
+const heightTriangle = (hipotenuse, cateto2) =>
+  Math.sqrt(hipotenuse * hipotenuse + cateto2 * cateto2);
+
+const calculateHypotenuseTriangle = () => {
+  const input1 = document.getElementById("isosceles1");
+  const input2 = document.getElementById("isosceles2");
+  const value1 = input1.value;
+  const value2 = input2.value;
+  const hypotenuses = heightTriangle(value1, value2);
+  alert(hypotenuses);
+};
+
 // Here we are connect HTML to JavaScript
 const calculateSquarePerimeter = () => {
   const input = document.getElementById("sideSquare");
@@ -36,9 +50,9 @@ const calculateTrianglePerimeter = () => {
   const input1 = document.getElementById("Triangle1");
   const input2 = document.getElementById("Triangle2");
   const input3 = document.getElementById("Triangle3");
-  const value1 = input1.value;
-  const value2 = input2.value;
-  const value3 = input3.value;
+  const value1 = parseInt(input1.value);
+  const value2 = parseInt(input2.value);
+  const value3 = parseInt(input3.value);
   const perimeter = perimeterTriangle(value1, value2, value3);
   alert(perimeter);
 };
@@ -49,5 +63,26 @@ const calculateTriangleArea = () => {
   const value1 = input1.value;
   const value2 = input2.value;
   const area = areaTriangle(value1, value2);
+  alert(area);
+};
+
+const calculateDiameterCircle = () => {
+  const input = document.getElementById("circle");
+  const value = input.value;
+  const diameter = diameterCircle(value);
+  alert(diameter);
+};
+
+const calculatePerimeterCircle = () => {
+  const input = document.getElementById("circle");
+  const value = input.value;
+  const perimeter = perimeterCircle(value);
+  alert(perimeter);
+};
+
+const calculateAreaCircle = () => {
+  const input = document.getElementById("circle");
+  const value = input.value;
+  const area = areaCircle(value);
   alert(area);
 };
