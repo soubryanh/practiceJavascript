8e4def7dc6eae2b485f6127221edcb57de42d551
bryanhernandz102 <bryanhernandz102@gmail.com>
2021-07-05 06:41:32 -0500

    second challenge

--- discount.js
+++ discount.js
@@ -5,12 +5,49 @@ const calculteDiscountPrice = (price, discount) => {
   //   console.log(discountPrice);
 };
 
-const onclickButtonPriceDiscount = () => {
+// const onclickButtonPriceDiscount = () => {
+//   const inputPrice = document.getElementById("price");
+//   const inputDiscount = document.getElementById("discount");
+//   const valuePrice = inputPrice.value;
+//   const valueDiscount = inputDiscount.value;
+//   const priceDiscount = calculteDiscountPrice(valuePrice, valueDiscount);
+//   const resultPrice = document.getElementById("resultPrice");
+//   resultPrice.innerText = `Discount: ${priceDiscount}`;
+// };
+
+onclickCalculateCupon = () => {
+  const coupons = [
+    {
+      name: "freetrial",
+      discount: 90,
+    },
+    {
+      name: "haflprice",
+      discount: 50,
+    },
+    {
+      name: "sexy",
+      discount: 20,
+    },
+  ];
   const inputPrice = document.getElementById("price");
-  const inputDiscount = document.getElementById("discount");
   const valuePrice = inputPrice.value;
-  const valueDiscount = inputDiscount.value;
-  const priceDiscount = calculteDiscountPrice(valuePrice, valueDiscount);
-  const resultPrice = document.getElementById("resultPrice");
-  resultPrice.innerText = `Discount: ${priceDiscount}`;
+
+  const inputCupon = document.getElementById("cupon");
+  const valueCupon = inputCupon.value;
+
+  const isCouponValueValid = (coupons) => {
+    return coupons.name === valueCupon;
+  };
+
+  const userCoupon = coupons.find(isCouponValueValid);
+
+  if (!userCoupon) {
+    alert(`El cupon ${valueCupon} no es valido`);
+  } else {
+    const discount = userCoupon.discount;
+    const priceDiscount = calculteDiscountPrice(valuePrice, discount);
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = `Result: ${priceDiscount}`;
+  }
 };
